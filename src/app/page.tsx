db78ff0090989115b9d5561867574cf24069cd0d
brenoluizdev <brenoluizpv3@gmail.com>
2024-12-20 14:02:14 -0300

    teste

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,7 +4,7 @@ import Link from 'next/link';
 const HomePage = () => {
   return (
     <div className="min-h-screen bg-red-100 flex items-center justify-center">
-      <h1 className="text-4xl font-bold text-red-600">Bem-vindo ao meu site!</h1>
+      <h1 className="text-4xl font-bold text-red-600">Bem-vindo ao meu site - teste!</h1>
     </div>
   );
 };
